51d39e5c7e149af9d0ae48d089b7f1cf824a3413
C, Meganathan <mc@allegion.com>
2024-11-19 19:48:44 +0530

    Clear app data for android

--- Appium Wizard/AndroidMethods.cs
+++ Appium Wizard/AndroidMethods.cs
@@ -403,6 +403,16 @@ namespace Appium_Wizard
             ExecuteCommandWithCmd("-s " + udid + " shell cmd statusbar collapse");
         }
 
+        public bool ClearAppData(string udid, string packageName)
+        {
+            var output = ExecuteCommandWithCmd("-s " + udid + " shell pm clear "+packageName);
+            if (output.Contains("Success"))
+            {
+                return true;
+            }
+            return false;
+        }
+
         public List<string> GetListOfInstalledApps(string udid)
         {
             var output = ExecuteCommand("-s " + udid + " shell pm list packages -3 --user 0");
